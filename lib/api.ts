d5e3285edbe28ca10851002a5294a6de021ad537
Martinez Muzela <martinezmuzela99@gmail.com>
2025-08-15 00:37:49 +0200

    mise a jour v5

--- lib/api.ts
+++ lib/api.ts
@@ -5,7 +5,7 @@ export interface ApiOptions extends RequestInit {
 
 // Configuration de l'API
 const API_CONFIG = {
-    baseUrl: process.env.BACK_PUBLIC_API_URL,
+    baseUrl: process.env.NEXT_PUBLIC_BACK_PUBLIC_API_URL,
     timeout: 10000,
     retries: 3,
 } as const
